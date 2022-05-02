1033eb3dde3f3ece191b142102e447c80f8787b8
White_Bishop <Shlepkinaleksandr@mail.ru>
2022-05-02 16:43:36 +0300

    create project

---
+++ ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GBProject
+//
+//  Created by Mac on 02.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
